ed6eb204cf06997345ffd10a6d48449a5fc52fa7
jyothi0301 <jyothiprakash.bandaru@gmail.com>
2025-08-06 08:47:26 +0530

    Add files via upload

---
+++ Ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "Prakash" {
+    count = 2
+  ami           = "ami-0f918f7e67a3323f0"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terra_EC2-${count.index + 1}"
+  }
+  }
