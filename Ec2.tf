b26c44861af7b0d61cd934c4374e27cbb0c2aa20
jyothi0301 <jyothiprakash.bandaru@gmail.com>
2025-11-04 11:45:56 +0530

    Update AWS region and instance count in Ec2.tf
    Changed to south 2

--- Ec2.tf
+++ Ec2.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-2"
 }
 
 resource "aws_instance" "Prakash" {
-    count = 4
+    count = 2
   ami           = "ami-0f918f7e67a3323f0"
   instance_type = "t3.micro"
 
@@ -12,6 +12,7 @@ resource "aws_instance" "Prakash" {
   }
 
   }
+
 
 
 
